ee246e05c3451c403a6ead9dbf007910f3c7e7f2
SergieAGA2 <sergiearruda@gmail.com>
2023-09-23 14:32:39 +0100

    Handle auth

--- .svelte-kit/ambient.d.ts
+++ .svelte-kit/ambient.d.ts
@@ -42,7 +42,7 @@ declare module '$env/static/private' {
 	export const ComSpec: string;
 	export const DriverData: string;
 	export const EDITOR: string;
-	export const EFC_15052: string;
+	export const EFC_5488: string;
 	export const GIT_ASKPASS: string;
 	export const HOME: string;
 	export const HOMEDRIVE: string;
@@ -156,7 +156,7 @@ declare module '$env/dynamic/private' {
 		ComSpec: string;
 		DriverData: string;
 		EDITOR: string;
-		EFC_15052: string;
+		EFC_5488: string;
 		GIT_ASKPASS: string;
 		HOME: string;
 		HOMEDRIVE: string;
